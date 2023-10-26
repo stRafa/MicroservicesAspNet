c538119b119893fcfa27dc88cb1ed61d9cccd7e5
Rafael Marques <rafaelmarques.ti@belenus.com.br>
2023-10-26 17:50:19 -0300

    feat: Added mongo catalog context

---
+++ src/Services/Catalog/BlankStore.Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using BlankStore.Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace BlankStore.Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
--- src/Services/Catalog/BlankStore.Catalog.API/Startup.cs
+++ src/Services/Catalog/BlankStore.Catalog.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +26,7 @@ namespace BlankStore.Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IMongoClient>(serviceProvider => new MongoClient(Configuration.GetValue<string>("DatabaseSettings:ConnectionString")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
